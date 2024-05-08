c5f87afebdb647076437af67784f2ed1aff4780c
jtanquil <jose.tanquiilut@gmail.com>
2024-05-07 18:32:02 -0700

    added Point, Line classes

--- graphics.py
+++ graphics.py
@@ -24,3 +24,19 @@ class Window:
 
   def close(self):
     self.is_running = False
+
+  def draw_line(self, line, fill_color):
+    line.draw(self.canvas, fill_color)
+
+class Point:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+class Line:
+  def __init__(self, start_point, end_point):
+    self.start_point = start_point
+    self.end_point = end_point
+
+  def draw(self, canvas, fill_color = "black"):
+    canvas.create_line(self.start_point.x, self.start_point.y, self.end_point.x, self.end_point.y, fill = fill_color, width = 2)
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 if __name__ == "__main__":
   win = Window(800, 600)
+  
+  start = Point(100, 100)
+  end = Point(300, 300)
+  line = Line(start, end)
+
+  win.draw_line(line = line, fill_color = "black")
+
   win.wait_for_close()
